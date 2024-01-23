3d15f5ea2c24d57e295cd56a0967599cad59fbee
NataliyaJilina <nataliyajilina@gmail.com>
2024-01-23 15:09:03 +0500

    для провверки

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 // Инпуты
 let userName=document.querySelector(".userName")
 let lastName=document.querySelector(".lastName")
-let email=document.querySelector(".email")
-let password1=document.querySelectorAll(".password")[0]
-let password2=document.querySelectorAll(".password")[1]
+let userEmail=document.querySelector(".userEmail")
+let userPassword1=document.querySelectorAll(".userPassword")[0]
+let userPassword2=document.querySelectorAll(".userPassword")[1]
 
 // Кнопки
 let btn = document.querySelector(".btn")
@@ -11,17 +11,59 @@ let btn = document.querySelector(".btn")
 btn.addEventListener("click", clickme)
 // userName.addEventListener("input", inputValue)
 
+let userNameReg = /^[A-Za-zА-Яа-я\- ]*[^0-9][A-Za-zА-Яа-я\- ]*$/;
+let lastNameReg = /^[A-Za-zА-Яа-я\- ]*[^0-9][A-Za-zА-Яа-я\- ]*$/;
+let emailReg= /^[\w\.-]+@[a-zA-Z\d\.-]+\.[a-zA-Z]{2,}$/
+let password1Reg= /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,20}$/
+let password2Reg= /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,20}$/
 
 
+ function clickme(event){
+     event.preventDefault()
+    // alert("я сработал")
+    validate(userNameReg,userName)
+    // validate1()
+    // validate2()
+    // validate3()
+    // validate4()
+ }
 
-function clickme(event){
-    event.preventDefault()
-    alert("я сработал")
-}
-// function inpurValue(){
+// function inputValue(){
     // console.log(userName.value)
 // }
 
-function validate(){
-   if(!userNameReg.test(userName.value)){} 
+function validate(userReg,userValue){
+   if(!userReg.test(userValue)){
+    userValue.classList.add("error")
+   }else{
+     if(userReg.test(userValue)){
+        userValue.classList.remove("error")
+
+   }
+}   
 }
+// function validate1(){
+    // if(!lastNameReg.test(lastName.value)){
+    //  lastName.classList.add("error")
+    // }else{
+        // if(lastNameReg.test(lastName.value)){
+        // lastName.classList.remove("error")
+    // }
+//  }   
+// }
+
+// function validate2(){
+    // if(!emailReg.test(email.value)){
+        // email.classList.add("error")
+    // }
+//  }   
+// function validate3(){
+    // if(!password1Reg.test(password1.value)){
+        // password1.classList.add("error")
+    // }
+//  }   
+// function validate4(){
+    // if(!password2Reg.test(password2Reg.value)){
+        // password2.classList.add("error")
+    // }
+//  }   
